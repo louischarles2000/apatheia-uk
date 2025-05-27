e19cbe6f2acb3ef5b0247c9d5a9d73f5732c2f4e
root <root@ip-172-31-36-24.eu-north-1.compute.internal>
2025-05-27 10:18:02 +0000

    Removed the alphaelemental image link

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
 // Todo: Change to shop.apatheia.org.uk
-export const IMAGE_PREFIX = process.env.NEXT_PUBLIC_OPENCART_IMAGES || 'https://shop.alphaelemental.com/opencart/image';
+export const IMAGE_PREFIX = process.env.NEXT_PUBLIC_OPENCART_IMAGES || 'https://shop.apatheia.org.uk/opencart/image';
